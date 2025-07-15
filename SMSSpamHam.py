877e23aae2502ead883f015adcb41a3ac979a084
Koloina Rajaonarivony <koloina.arilala@gmail.com>
2025-07-15 08:59:57 +0300

    creation model

--- SMSSpamHam.py
+++ SMSSpamHam.py
@@ -2,6 +2,9 @@ import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+import joblib
 
 df = pd.read_csv("SMSSpamCollection", sep = '\t', header= None, names= ['label', 'message'])
 print(df.head())
@@ -18,3 +21,27 @@ y = df['label']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state=42)
 
 #entrainement du modele
+model = MultinomialNB()
+model.fit(X_train, y_train)
+
+#Prediction
+y_pred = model.predict(X_test)
+
+#Evaluation
+print("Accuracy:", accuracy_score(y_test, y_pred))
+print(confusion_matrix(y_test, y_pred))
+print(classification_report(y_test, y_pred))
+
+#test manuel
+sample = ["Congratulations! You'vre won a freee iPhone. CLick here to claim now!",
+"Salut, peux-tu me rappeler dès que possible?"]
+
+sample_vectorized = vectorizer.transform(sample)
+predictions = model.predict(sample_vectorized)
+
+for text, pred in zip(sample, predictions):
+    print(f"Message: {text} \n=> {'Spam' if pred == 1 else 'Ham'}\n")
+
+#sauvegarde du modele
+joblib.dump(model, 'spam_classifer.pkl')
+joblib.dump(vectorizer, 'vectorizer.pkl')
